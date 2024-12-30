0caf288615cfd746f353f3dbe1624f7f74a7fa0d
wahyudiaditya <wahyudiaditya007@gmail.com>
2024-12-30 17:10:12 +0700

    feat:add cors to sever-side

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
 
