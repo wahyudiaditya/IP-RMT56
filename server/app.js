e8259caa2b2817d730facde780a3e8c6b30432c5
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-02-10 12:50:14 +0700

    feat: refactor urlencoded

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(router);
